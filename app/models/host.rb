29d5965bf6848ac1e2e0f1db6555513de1d7cc88
Eduardo Ramos <ramos.eduardo87@gmail.com>
2015-07-21 02:29:46 -0300

    Added validations.

--- app/models/host.rb
+++ app/models/host.rb
@@ -1,2 +1,5 @@
 class Host < ActiveRecord::Base
+  validates :hostname, presence: true
+  validates :macaddress, presence: true, length: { is: 17 }
+  validates :ipaddress, presence: true, length: { maximum: 15 }
 end
