5015a2852a57759ef7551b6b1816625f5748de5e
Eduardo Ramos <ramos.eduardo87@gmail.com>
2015-07-20 22:28:17 -0300

    Generated model.

---
+++ db/migrate/20150721011950_create_hosts.rb
@@ -0,0 +1,14 @@
+class CreateHosts < ActiveRecord::Migration
+  def change
+    create_table :hosts do |t|
+      t.string :hostname
+      t.string :macaddress
+      t.string :ipaddress
+
+      t.timestamps null: false
+    end
+    add_index :hosts, :hostname, :unique => true
+    add_index :hosts, :macaddress, :unique => true
+    add_index :hosts, :ipaddress, :unique => true
+  end
+end
---
+++ db/schema.rb
@@ -0,0 +1,28 @@
+# encoding: UTF-8
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# Note that this schema.rb definition is the authoritative source for your
+# database schema. If you need to create the application database on another
+# system, you should be using db:schema:load, not running all the migrations
+# from scratch. The latter is a flawed and unsustainable approach (the more migrations
+# you'll amass, the slower it'll run and the greater likelihood for issues).
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 20150721011950) do
+
+  create_table "hosts", force: :cascade do |t|
+    t.string   "hostname"
+    t.string   "macaddress"
+    t.string   "ipaddress"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "hosts", ["hostname"], name: "index_hosts_on_hostname", unique: true
+  add_index "hosts", ["ipaddress"], name: "index_hosts_on_ipaddress", unique: true
+  add_index "hosts", ["macaddress"], name: "index_hosts_on_macaddress", unique: true
+
+end
