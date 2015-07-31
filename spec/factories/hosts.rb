5c3b6a48fdc6308cc4c4fd57d3d3587518f835b3
Eduardo Ramos <eduardo@freedominterface.org>
2015-07-31 00:17:32 -0300

    Fixed register_host_spec.rb with Domain model.

---
+++ spec/factories/hosts.rb
@@ -0,0 +1,9 @@
+FactoryGirl.define do
+  factory :host do
+    hostname "MyString"
+macaddress "MyString"
+ipaddress "MyString"
+domain nil
+  end
+
+end
--- spec/support/factories.rb
+++
@@ -1,9 +0,0 @@
-require 'factory_girl'
-
-FactoryGirl.define do
-  factory :host do
-    hostname "localhost"
-    macaddress "22:33:44:55:66:77"
-    ipaddress "192.168.1.1"
-  end
-end
